1c3da64afc73187a778a94afa29cacd6ee54565a
Nicole <nicolejardim.c@gmail.com>
2025-06-10 20:29:58 -0300

    Add files via upload

---
+++ scripts/level_end.gd
@@ -0,0 +1,17 @@
+extends Area2D
+
+
+# Called when the node enters the scene tree for the first time.
+func _ready() -> void:
+	pass # Replace with function body.
+
+
+# Called every frame. 'delta' is the elapsed time since the previous frame.
+func _process(delta: float) -> void:
+	pass
+
+
+func _on_body_entered(body: Node2D) -> void:
+	if body.is_in_group("player"):
+		await get_tree().create_timer(3.0).timeout
+		get_tree().change_scene_to_file("res://scenes/winter_scene.tscn")
